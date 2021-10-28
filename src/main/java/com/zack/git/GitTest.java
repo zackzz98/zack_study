6f0971bf8c09f4a2b9e260c7d257991e2a778103
zack <zack98zy@163.com>
2021-10-28 14:52:08 +0800

    hot-fix first commit

--- src/main/java/com/zack/git/GitTest.java
+++ src/main/java/com/zack/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Flink");
+        System.out.println("hot-fix first");
     }
 }
