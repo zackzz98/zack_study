c4a24760f912e965506dc1931b6373b550997a64
zack <zack98zy@163.com>
2021-10-28 14:49:23 +0800

    first commit

---
+++ src/main/java/com/zack/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zack.git;
+
+/**
+ * @auther zack_zhang
+ * @data 2021/10/28 14:38
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
