3baa53a0042b1bce233284c43291781356e50ce0
zack <zack98zy@163.com>
2021-10-28 14:55:54 +0800

    master first commit

--- src/main/java/com/zack/git/GitTest.java
+++ src/main/java/com/zack/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Flink");
         System.out.println("hot-fix first");
+        System.out.println("master first");
     }
 }
